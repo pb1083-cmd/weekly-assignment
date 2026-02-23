c42cda27ad02e0321f0c40b1e9f15583e4d0450d
Prahlad Somangoud Biradar <pb1083@srmist.edu.in>
2026-02-24 00:36:56 +0530

    sample question

---
+++ sp3.java
@@ -0,0 +1,22 @@
+public class StudentDetails {
+
+    public static void main(String[] args) {
+
+        String name = "Sam";
+        int rollNumber = 1;
+        double percentMarks = 99.99;
+        char result = 'P';   // 'P' for Pass, 'F' for Fail
+
+        System.out.println("Student Name  : " + name);
+        System.out.println("Roll Number   : " + rollNumber);
+        System.out.println("Percent Marks : " + percentMarks);
+        System.out.println("Result        : " + result);
+
+        // Checking Pass or Fail
+        if (result == 'P') {
+            System.out.println("Status        : Pass");
+        } else {
+            System.out.println("Status        : Fail");
+        }
+    }
+}
