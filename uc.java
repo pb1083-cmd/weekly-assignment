74355e4344df8b136b29bc26080a62173ab84509
Prahlad Somangoud Biradar <pb1083@srmist.edu.in>
2026-03-09 23:01:04 +0530

    WEEKLY ASSIGNMENT

---
+++ uc.java
@@ -0,0 +1,52 @@
+public class Banner {
+
+    // Method for letter O
+    public static String[] getOPattern() {
+        return new String[] {
+            " *** ",
+            "**  **",
+            "**  **",
+            "**  **",
+            "**  **",
+            "**  **",
+            " *** "
+        };
+    }
+
+    // Method for letter P
+    public static String[] getPPattern() {
+        return new String[] {
+            "**** ",
+            "** **",
+            "** **",
+            "**** ",
+            "**   ",
+            "**   ",
+            "**   "
+        };
+    }
+
+    // Method for letter S
+    public static String[] getSPattern() {
+        return new String[] {
+            " ****",
+            "**   ",
+            "**   ",
+            " *** ",
+            "    **",
+            "    **",
+            "**** "
+        };
+    }
+
+    public static void main(String[] args) {
+
+        String[] oPattern = getOPattern();
+        String[] pPattern = getPPattern();
+        String[] sPattern = getSPattern();
+
+        for (int i = 0; i < oPattern.length; i++) {
+            System.out.println(oPattern[i] + "  " + pPattern[i] + "  " + sPattern[i]);
+        }
+    }
+}
